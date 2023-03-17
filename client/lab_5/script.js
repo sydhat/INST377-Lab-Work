21ba8d84c6a22c700b093d28b742105cb7675617
sydhat <59001958+sydhat@users.noreply.github.com>
2023-03-17 10:48:17 -0400

    updated lab5 script

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -3,11 +3,30 @@
   by adding `<script src="script.js">` just before your closing `</body>` tag
 */
 
+/* A quick filter that will return something based on a matching input */
+function filterList(list, query) {
+  /*
+    Using the .filter array method, 
+    return a list that is filtered by comparing the item name in lower case
+    to the query in lower case
+    Ask the TAs if you need help with this
+  */
+}
+
 async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
-    submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
-    console.log('form submission'); // this is substituting for a "breakpoint"
+  const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  // Add a querySelector that targets your filter button here
+
+  let currentList = []; // this is "scoped" to the main event function
+  
+  /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
+  mainForm.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+    
+    // This prevents your page from becoming a list of 1000 records from the county, even if your form still has an action set on it
+    submitEvent.preventDefault(); 
+    
+    // this is substituting for a "breakpoint" - it prints to the browser to tell us we successfully submitted the form
+    console.log('form submission'); 
 
     /*
       ## GET requests and Javascript
@@ -24,11 +43,26 @@ async function mainEvent() { // the async keyword means we can make API requests
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
 
     // This changes the response from the GET into data we can use - an "object"
-    const arrayFromJson = await results.json();
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it initially contains all 1,000 records from your request
+    currentList = await results.json();
+
+    /*
+      This array initially contains all 1,000 records from your request,
+      but it will only be defined _after_ the request resolves - any filtering on it before that
+      simply won't work.
+    */
+    console.table(currentList); 
   });
+
+
+  /*
+    Now that you HAVE a list loaded, write an event listener set to your filter button
+    it should use the 'new FormData(target-form)' method to read the contents of your main form
+    and the Object.fromEntries() method to convert that data to an object we can work with
+    When you have the contents of the form, use the placeholder at line 7
+    to write a list filter
+    Fire it here and filter for the word "pizza"
+    you should get approximately 46 results
+  */
 }
 
 /*
